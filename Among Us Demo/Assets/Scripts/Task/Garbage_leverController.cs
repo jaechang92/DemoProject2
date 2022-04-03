beb16438dc2cda22e1d734d174645a42173d16cd
jaechang92 <jaechang0721k@gmail.com>
2022-04-04 00:15:27 +0900

    쓰레기통미션

---
+++ Among Us Demo/Assets/Scripts/Task/Garbage_leverController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garbage_leverController : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform garbage_leverBars;
+
+    [SerializeField]
+    private RectTransform garbage_leverHandle;
+
+    [SerializeField]
+    private bool isSelected = false;
+    private float originSizeY;
+
+    [SerializeField]
+    private Collider2D garbage_Door;
+
+    [SerializeField]
+    private List<Rigidbody2D> leafs_rb;
+    [SerializeField]
+    private float forceValue;
+    [SerializeField]
+    private CameraShake shakeParent;
+    void Start()
+    {
+        // 레버의 바 길이 제한 걸기
+        originSizeY = garbage_leverBars.sizeDelta.y;
+        if (shakeParent == null)
+        {
+            shakeParent = GetComponentInParent<CameraShake>();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.right, 1f);
+            if (hit.collider != null && hit.collider.CompareTag("ClickObject"))
+            {
+                isSelected = true;
+                garbage_Door.isTrigger = true;
+
+                foreach (var leaf in leafs_rb)
+                {
+                    leaf.AddRelativeForce(Vector2.up * forceValue);
+                    leaf.AddForce(Vector2.up * forceValue);
+                    shakeParent.shakeDuration = 2.0f;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSelected = false;
+            garbage_leverBars.sizeDelta = new Vector2(garbage_leverBars.sizeDelta.x, originSizeY);
+            garbage_leverBars.localRotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            garbage_Door.isTrigger = false;
+        }
+
+
+        if (isSelected)
+        {
+            float angle = Vector2.SignedAngle(transform.position + Vector3.right - transform.position, Input.mousePosition - transform.position);
+            Debug.Log(angle);
+
+            
+
+            float dist = garbage_leverBars.transform.position.y > Input.mousePosition.y ? garbage_leverBars.transform.position.y - Input.mousePosition.y : Input.mousePosition.y - garbage_leverBars.transform.position.y;
+            //Vector2.Distance(garbage_leverBars.transform.position, Input.mousePosition)-27;
+            dist -= 27;
+
+            dist = Mathf.Clamp(dist, 0, originSizeY);
+            angle = angle > 0 ? 180 : 0;
+            Debug.Log(dist);
+            garbage_leverBars.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            garbage_leverBars.sizeDelta = new Vector2(garbage_leverBars.sizeDelta.x, dist);
+        }
+
+
+    }
+}
