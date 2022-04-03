c90ff1ff57df30993df2ca6079230d682490c302
JaeChang <jaechang0721k@gmail.com>
2022-04-03 18:08:57 +0900

    미션

---
+++ Among Us Demo/Assets/Scripts/Task/FuelMission.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class FuelMission : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private Image fuelGage;
+
+    [SerializeField]
+    private float fuelGageTimer;
+
+    [SerializeField]
+    private bool isClear = false;
+    [SerializeField]
+    private bool isPress = false;
+
+    void Update()
+    {
+        if (!isClear)
+        {
+            GetFuel();
+        }
+
+    }
+
+    public void GetFuel()
+    {
+        if (isPress)
+        {
+            fuelGage.fillAmount +=  Time.deltaTime / fuelGageTimer;
+        }
+        else
+        {
+            fuelGage.fillAmount -= Time.deltaTime / fuelGageTimer;
+        }
+
+        if (fuelGage.fillAmount >= 1)
+        {
+            isClear = true;
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.pointerCurrentRaycast.gameObject.CompareTag("UIButton"))
+        {
+            isPress = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPress = false;
+    }
+}
