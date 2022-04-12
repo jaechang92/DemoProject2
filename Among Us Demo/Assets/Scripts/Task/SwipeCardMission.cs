09f973a2238b34a1dfe8d681c068b2809d4b2756
JaeChang <jaechang0721k@gmail.com>
2022-04-12 16:05:07 +0900

    어드민 미션

--- Among Us Demo/Assets/Scripts/Task/SwipeCardMission.cs
+++ Among Us Demo/Assets/Scripts/Task/SwipeCardMission.cs
@@ -11,17 +11,60 @@ public class SwipeCardMission : MonoBehaviour
     [SerializeField]
     private RectTransform card;
 
+    [SerializeField]
+    private float min, max;
 
+    private float currentTime = 0;
+    private bool isChecked = false;
+    private bool isClear = false;
     private void OnEnable()
     {
         card.anchoredPosition = new Vector2(cardStartPoint.x, cardStartPoint.y);
         card.localScale = new Vector3(.8f, .8f, 1);
+
+    }
+
+    private void Update()
+    {
+        card.anchoredPosition = Vector2.MoveTowards(card.anchoredPosition, cardEndPoint, 300 * Time.deltaTime);
+        card.localScale = Vector3.MoveTowards(card.localScale, Vector3.one, Time.deltaTime);
+        if (isChecked)
+        {
+            currentTime += Time.deltaTime;
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            card.anchoredPosition= new Vector2(Input.mousePosition.x, card.anchoredPosition.y);
+        }
     }
 
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject == card.gameObject)
+        {
+            isChecked = true;
+            currentTime = 0;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == card.gameObject)
+        {
+            isChecked = false;
+            if (currentTime >= min && currentTime <= max)
+            {
+                isClear = true;
+                Invoke("CloseUI", 1.0f);
+            }
+        }
+    }
+
+    private void CloseUI()
+    {
+        gameObject.SetActive(false);
     }
 }
