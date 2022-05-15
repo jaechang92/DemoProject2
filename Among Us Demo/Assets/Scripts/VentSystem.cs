032de1e7653e8b1bc83127050c190796cc7c9ab3
JaeChang <jaechang0721k@gmail.com>
2022-05-15 17:24:52 +0900

    벤트 작업

---
+++ Among Us Demo/Assets/Scripts/VentSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentSystem : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> ventCafeteriaAdminHallway;
+    [SerializeField]
+    private List<Transform> ventMedbayElectricalSecurity;
+    [SerializeField]
+    private List<Transform> ventReactorUpperEngine;
+    [SerializeField]
+    private List<Transform> ventReactorLowerEngine;
+    [SerializeField]
+    private List<Transform> ventNavigationWeapons;
+    [SerializeField]            
+    private List<Transform> ventNavigationShields;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
